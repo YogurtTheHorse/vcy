4d7725cbb8fc1907750dbad8d43f9cb756041c80
YogurtTheHorse <yegorf1@gmail.com>
2019-08-03 18:17:01 +0300

    Fix greetings screen

--- vcy/screens/etc/greetings_screen.py
+++ vcy/screens/etc/greetings_screen.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from vcy import text_utils
-from vcy.entities import InputMessage
+from vcy.entities import InputMessage, Answer
 from vcy.managers import screen_manager
 from vcy.models import Chat
 from vcy.screens.screen import Screen
@@ -13,13 +15,16 @@ class GreetingsScreen(Screen):
     def get_name():
         return 'greetings'
 
+    def on_open(self) -> Optional[Answer]:
+        if self.chat.first_play:
+            self.chat.first_play = False
+            return self.answer('Привет, кажется ты играешь первый раз, не хочешь узнать, что нужно сделать?')
+        else:
+            return self.answer('Привет, давно не виделись. Напомнить правила игры?')
+
     def process_message(self, message: InputMessage):
         if message.is_start_message:
-            if self.chat.first_play:
-                self.chat.first_play = False
-                return message.answer('Привет, кажется ты играешь первый раз, не хочешь узнать, что нужно сделать?')
-            else:
-                return message.answer('Привет, давно не виделись. Напомнить правила игры?')
+            return self.on_open()
         else:
             new_screen = 'tutorial' if text_utils.is_positive(message.text) else 'start_game'
             return screen_manager.switch_screen(self.chat, new_screen)
