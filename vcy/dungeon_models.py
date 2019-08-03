ce31f46785748025b517b7745d1a6c4e254a0bbf
YogurtTheHorse <yegorf1@gmail.com>
2019-08-03 21:32:41 +0300

    Add rooms to door

--- vcy/dungeon_models.py
+++ vcy/dungeon_models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from mongoengine import EmbeddedDocument, EmbeddedDocumentListField, IntField, StringField
 
 from vcy.entities import GameColors
@@ -12,6 +14,10 @@ class Room(EmbeddedDocument):
 
 class Door(EmbeddedDocument):
     id = IntField()
+
+    first_room_id = IntField()
+    second_room_id = IntField()
+
     color = StringField(choices=str_colors)
 
 
@@ -21,9 +27,9 @@ class Key(EmbeddedDocument):
 
 
 class Dungeon(EmbeddedDocument):
-    rooms = EmbeddedDocumentListField(Room, default=list)
-    doors = EmbeddedDocumentListField(Door, default=list)
-    keys = EmbeddedDocumentListField(Key, default=list)
+    rooms = EmbeddedDocumentListField(Room, default=list)  # type: List[Room]
+    doors = EmbeddedDocumentListField(Door, default=list)  # type: List[Door]
+    keys = EmbeddedDocumentListField(Key, default=list)  # type: List[Key]
 
     finish_room_id = IntField()
 
